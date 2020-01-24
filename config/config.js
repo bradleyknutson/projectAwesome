e1009e82ae3b3ea92d48d4a2ea2db8de7d47d2a1
Bradley Knutson <bradley@bknutson.com>
2020-01-24 09:24:04 -0700

    Modified config.json to use .env

---
+++ config/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "mysql"
+  },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "mysql"
+  },
+  production: {
+    use_env_variable: process.env.JAWSDB_URL,
+    dialect: "mysql"
+  }
