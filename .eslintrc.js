342d703cc2fceea7106769cc94671f51159b0156
Bradley Knutson <bradley@bknutson.com>
2020-01-24 11:36:54 -0700

    Updated ESLint and rules

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'es6': true,
+        'node': true,
+        'commonjs': true
+	},
+	'extends': 'eslint:recommended',
+	'globals': {
+		'Atomics': 'readonly',
+		'SharedArrayBuffer': 'readonly'
+	},
+	'parserOptions': {
+		'ecmaVersion': 2018,
+		'sourceType': 'module'
+	},
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'backtick'
+		],
+		'semi': [
+			'error',
+			'always'
+        ],
+        'no-duplicate-case':'error',
+        'no-empty':'error',
+        'no-extra-semi':'error',
+        'no-func-assign': 'error',
+        'no-irregular-whitespace':'error',
+        'no-unreachable': 'error',
+        'curly': 'error',
+        'dot-notation': 'error',
+        'eqeqeq': 'error',
+        'no-empty-function': 'error',
+        'no-multi-spaces': 'error',
+        'no-unused-vars': 'error',
+        'camelcase': 'error',
+	}
+};
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-require("dotenv").config();
-var express = require("express");
-var exphbs = require("express-handlebars");
+require(`dotenv`).config();
+var express = require(`express`);
+var exphbs = require(`express-handlebars`);
 
-var db = require("./models");
+var db = require(`./models`);
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -10,38 +10,38 @@ var PORT = process.env.PORT || 3000;
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static(`public`));
 
 // Handlebars
 app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
+	`handlebars`,
+	exphbs({
+		defaultLayout: `main`
+	})
 );
-app.set("view engine", "handlebars");
+app.set(`view engine`, `handlebars`);
 
 // Routes
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require(`./routes/apiRoutes`)(app);
+require(`./routes/htmlRoutes`)(app);
 
 var syncOptions = { force: false };
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
-if (process.env.NODE_ENV === "test") {
-  syncOptions.force = true;
+if (process.env.NODE_ENV === `test`) {
+	syncOptions.force = true;
 }
 
 // Starting the server, syncing our models ------------------------------------/
 db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
-  });
+	app.listen(PORT, function() {
+		console.log(
+			`==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.`,
+			PORT,
+			PORT
+		);
+	});
 });
 
 module.exports = app;
