633df4a1cfe963bd6618eceddc2ddde784dee53c
Bradley Knutson <bradley@bknutson.com>
2020-01-24 14:41:14 -0700

    Created initial user model with email and password

---
+++ models/user.js
@@ -0,0 +1,31 @@
+const bcrypt = require(`bcryptjs`);
+
+module.exports = function(sequelize, DataTypes) {
+    const User = sequelize.define(`User`, {
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    });
+
+    User.prototype.validPassword = function(password) {
+        return bcrypt.compareSync(password, this.password);
+    };
+
+    User.beforeCreate(user => {
+        user.password = bcrypt.hashSync(
+            user.password,
+            bcrypt.genSaltSync(12),
+            null
+        );
+    });
+    return User;
+};
