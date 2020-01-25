43ceeba02f6757d46bb38cb668c20b0da4cb7109
Bradley Knutson <bradley@bknutson.com>
2020-01-24 18:07:45 -0700

    Changed backticks to double quotes in cofig.js

--- config/config.js
+++ config/config.js
@@ -4,17 +4,17 @@ module.exports = {
 		password: process.env.DB_PASS,
 		database: process.env.DB_NAME,
 		host: process.env.DB_HOST,
-		dialect: `mysql`
+		dialect: "mysql"
 	},
 	test: {
 		username: process.env.DB_USER,
 		password: process.env.DB_PASS,
 		database: process.env.DB_NAME,
 		host: process.env.DB_HOST,
-		dialect: `mysql`
+		dialect: "mysql"
 	},
 	production: {
 		use_env_variable: process.env.JAWSDB_URL,
-		dialect: `mysql`
+		dialect: "mysql"
 	}
 };
