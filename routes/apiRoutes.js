14f5039c892055ba71505d943efb369e77d30290
Stacy Holtz <stacyholtz6@gmail.com>
2020-01-25 14:00:50 -0700

    added form to profile

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,10 @@
 var db = require(`../models`);
 const passport = require(`../config/passport`);
+const bodyParser = require(`body-parser`);
 
-module.exports = function(app) {
+
+
+module.exports = function (app) {
     app.post(`/api/signup`, (req, res) => {
         db.User.create({
             email: req.body.email,
@@ -15,6 +18,12 @@ module.exports = function(app) {
 
     app.post(`/api/login`, passport.authenticate(`local`), (req, res) => {
         res.json(`/members`);
+    });
+
+    // add profile picture
+    app.use(bodyParser.json()).use(bodyParser.urlencoded({ extended: true }));
+    app.post(`/profile/image`, (req, res) => {
+        res.send(req.body);
     });
 };
 
