81d23165bb3dccc251387f13f06ed4457d336d82
Bradley Knutson <bradley@bknutson.com>
2020-01-24 18:35:20 -0700

    Modified index.js to better use JAWSDB

--- models/index.js
+++ models/index.js
@@ -8,16 +8,11 @@ var env = process.env.NODE_ENV || "development";
 var config = require(__dirname + "/../config/config.js")[env];
 var db = {};
 
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+let sequelize;
+if (process.env.JAWSDB_URL) {
+  let sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  var sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config.dialect,
-    config
-  );
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
 fs.readdirSync(__dirname)
