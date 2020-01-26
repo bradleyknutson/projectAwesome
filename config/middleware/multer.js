51cf95dcfb79cb7820f79f206a5332e3814c28a0
Stacy Holtz <stacyholtz6@gmail.com>
2020-01-26 12:59:24 -0700

    added cloudinary login and deleted index.js

--- config/middleware/multer.js
+++ config/middleware/multer.js
@@ -1,10 +1,10 @@
 const multer = require(`multer`);
 
 module.exports = multer({
-    // eslint-disable-next-line indent
-  storage: multer.diskStorage({}),
+ 
+    storage: multer.diskStorage({}),
     fileFilter: (req, file, cb) => {
-        if(!file.mimtype.match(/jpe|jpeg|png$i/)){
+        if(!file.mimetype.match(/jpe|jpeg|png$i/)){
             cb(new Error(`File is not supported`), false);
             return; 
         }
