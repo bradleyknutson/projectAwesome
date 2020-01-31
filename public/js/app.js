cbdd5e4b1b2dfce43cfb5dc2d8311f0b782481ac
Stacy Holtz <stacyholtz6@gmail.com>
2020-01-30 19:03:58 -0700

    jquery for survey

--- public/js/app.js
+++ public/js/app.js
@@ -6,9 +6,9 @@ $(function () {
             password: $(`input[name="password"]`).val().trim()
         };
 
-        if(!userData.email || !userData.password){
+        if (!userData.email || !userData.password) {
             return;
-        }else{
+        } else {
             $.post(`/api/login`, userData).then((response) => {
                 $(`input[name="email"]`).val(``);
                 $(`input[name="password"]`).val(``);
@@ -26,9 +26,9 @@ $(function () {
             password: $(`input[name="password"]`).val().trim()
         };
 
-        if(!userData.email || !userData.password){
+        if (!userData.email || !userData.password) {
             return;
-        }else{
+        } else {
             $.post(`/api/signup`, userData).then((response) => {
                 $(`input[name="email"]`).val(``);
                 $(`input[name="password"]`).val(``);
@@ -38,6 +38,36 @@ $(function () {
             });
         }
     });
-    //  create logic for search form
-    
+    // get the search form data
+    $(`#search-form`).on(`submit`, event => {
+        event.preventDefault();
+        let searchData = {
+            name: $(`#name-input`).val().trim(),
+            type: $(`#type-input`).val().trim(),
+            breed: $(`#breed-input`).val(),
+            age: $(`#age-input`).val().trim(),
+            gender: $(`#gender-select`).val(),
+            children: $(`#children-select`).val(),
+            dogs: $(`#dogs-select`).val(),
+            cats: $(`#cats-select`).val(),
+            zip: $(`zip-input`).val().trim(),
+        };
+        // send post request & clear inputs
+        $.post(`/api/search`, searchData).then((res) => {
+            $(`#name-input`).val(``);
+            $(`#type-input`).val(``);
+            $(`#breed-input`).val(``);
+            $(`#age-input`).val(``);
+            $(`#gender-select`).val(``);
+            $(`#children-select`).val(``);
+            $(`#dogs-select`).val(``);
+            $(`#cats-select`).val(``);
+            $(`#zip-input`).val(``);
+            window.location.replace(res);
+        }).catch(err => {
+            console.log(`search form error`, err.resJSON);
+        });
+
+    });
+
 });
